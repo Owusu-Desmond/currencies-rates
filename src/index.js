ca6f9f37b2131cbd3e804964a46ebb856f46d342
Owusu-Desmond <desmond0406114009@gmail.com>
2022-08-31 12:34:23 +0000

    Connect app to redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './store/configureStore';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    ,
+  </Provider>,
+
 );
 
 // If you want to start measuring performance in your app, pass a function
