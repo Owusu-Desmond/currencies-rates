9696461adf3b3887e26390880af322c1a39a3739
Owusu-Desmond <desmond0406114009@gmail.com>
2022-08-31 12:33:25 +0000

    Create currencies store

---
+++ src/store/currencies/currencyByCode.js
@@ -0,0 +1,34 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const FETCH_CURRENCIES_BY_CURRENCY_CODE_LATEST = 'FETCH_CURRENCIES_BY_CURRENCY_CODE_LATEST';
+const FETCH_CURRENCIES_BY_CURRENCY_CODE__IN_SPECIIC_DATE = 'FETCH_CURRENCIES_BY_CURRENCY_CODE_LATEST_IN_SPECIIC_DATE';
+const CURRENCIES_BASE_API_URL = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1';
+
+export const fetchCurrencyByCodeLatest = createAsyncThunk(
+  FETCH_CURRENCIES_BY_CURRENCY_CODE_LATEST,
+  async (code) => {
+    const response = await fetch(`${CURRENCIES_BASE_API_URL}/latest/currencies/${code}.json`);
+    const data = await response.json();
+    return data;
+  },
+);
+
+export const fetchCurrencyByCodeLatestInSpecificDate = createAsyncThunk(
+  FETCH_CURRENCIES_BY_CURRENCY_CODE__IN_SPECIIC_DATE,
+  async (dateCodeObj) => {
+    const response = await fetch(`${CURRENCIES_BASE_API_URL}/${dateCodeObj.date}/currencies/${dateCodeObj.code}.json`);
+    const data = await response.json();
+    return data;
+  },
+);
+
+export default function currencyRateByCodeReducer(state = {}, action) {
+  switch (action.type) {
+    case `${FETCH_CURRENCIES_BY_CURRENCY_CODE_LATEST}/fulfilled`:
+      return action.payload;
+    case `${FETCH_CURRENCIES_BY_CURRENCY_CODE__IN_SPECIIC_DATE}/fulfilled`:
+      return action.payload;
+    default:
+      return state;
+  }
+}
