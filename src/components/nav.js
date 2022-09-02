73a2951398229841831a9c61d12b2b0ee46a24a6
Owusu-Desmond <desmond0406114009@gmail.com>
2022-09-02 22:55:23 +0000

    Update component to add filter feature

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,17 +1,24 @@
-import { FaSearch } from 'react-icons/fa';
 import { PropTypes } from 'prop-types';
-import { IoMdSettings } from 'react-icons/io';
+import { IoMdSettings, IoIosArrowBack } from 'react-icons/io';
+import { AiFillHome } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 const Nav = ({ children, text }) => (
   <nav className="all-currencies-nav">
     <div>
-      {children}
+      {(window.location.pathname === '/') ? (
+        <AiFillHome />
+      ) : (
+        <Link to="/">
+          <IoIosArrowBack />
+        </Link>
+      )}
     </div>
     <div className="nav-text">
       {text}
     </div>
     <div className="search-setting-icon">
-      <FaSearch />
+      {children}
       <IoMdSettings />
     </div>
   </nav>
