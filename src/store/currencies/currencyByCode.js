58d8b534ffc99f669dcbec0b2ed2d56511c3b3f3
Owusu-Desmond <desmond0406114009@gmail.com>
2022-09-02 22:51:20 +0000

    Update store state

--- src/store/currencies/currencyByCode.js
+++ src/store/currencies/currencyByCode.js
@@ -3,6 +3,8 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 const FETCH_CURRENCIES_BY_CURRENCY_CODE_LATEST = 'FETCH_CURRENCIES_BY_CURRENCY_CODE_LATEST';
 const FETCH_CURRENCIES_BY_CURRENCY_CODE_IN_SPECIIC_DATE = 'FETCH_CURRENCIES_BY_CURRENCY_CODE_LATEST_IN_SPECIIC_DATE';
 const CURRENCIES_BASE_API_URL = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1';
+const DISPLAY_SINGLE_RATE = 'DISPLAY_SINGLE_RATE';
+const DISPLAY_ALL_RATES = 'DISPLAY_ALL_RATES';
 
 export const fetchCurrencyByCodeLatest = createAsyncThunk(
   FETCH_CURRENCIES_BY_CURRENCY_CODE_LATEST,
@@ -22,12 +24,47 @@ export const fetchCurrencyByCodeLatestInSpecificDate = createAsyncThunk(
   },
 );
 
-export default function currencyRateByCodeReducer(state = {}, action) {
+export const displaySingleRate = (data) => ({
+  type: DISPLAY_SINGLE_RATE,
+  payload: data,
+});
+
+export const displayAllRates = () => ({
+  type: DISPLAY_ALL_RATES,
+});
+
+export default function currencyRateByCodeReducer(state = [{}, {}], action) {
   switch (action.type) {
     case `${FETCH_CURRENCIES_BY_CURRENCY_CODE_LATEST}/fulfilled`:
-      return action.payload;
+      return [
+        {
+          ...state[0],
+          ...action.payload,
+        },
+        {},
+      ];
     case `${FETCH_CURRENCIES_BY_CURRENCY_CODE_IN_SPECIIC_DATE}/fulfilled`:
-      return action.payload;
+      return [
+        {
+          ...state[0],
+          ...action.payload,
+        },
+        {},
+      ];
+    case DISPLAY_SINGLE_RATE:
+      return [
+        {
+          ...state[0],
+        },
+        {
+          [action.payload.rateCode]: state[0][action.payload.code][action.payload.rateCode],
+        },
+      ];
+    case DISPLAY_ALL_RATES:
+      return [
+        state[0],
+        {},
+      ];
     default:
       return state;
   }
